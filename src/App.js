fc3ffcdd2ff66b1e5017181480b6428490296a12
ciming <xuxcf2010@163.com>
2020-11-13 15:40:31 +0800

    初始化

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="row">
+        <div class="col-md-12">
+        dsadas
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-3">
+            qw
+          </div>
+          <div class="col-md-6">
+            wqw
+          </div>
+          <div class="col-md-3">
+            wqw
+          </div>
+          
+      </div>
     </div>
   );
 }
