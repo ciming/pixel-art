63cc0e01e5e7cbcdc198c86889eb159d1106c4bf
ciming <xuxcf2010@163.com>
2020-11-16 15:00:14 +0800

    添加全局转态

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore, createHooks } from "react-global-hook"
+
+const initialState = {
+  currentColor: null,
+  pixelData: []
+}
+
+const actions = ({ setState, getState }) => ({
+  setCurrentColor(color) {
+    setState({
+      currentColor: color
+    })
+  }
+})
+
+export const Store = createStore(initialState, actions);
+
+export const useStore = createHooks(Store);
